ca12e2e225792134a9d9491445eb8f1db72185fc
rodrigo <rocasgut@gmail.com>
2024-11-14 20:24:28 +0100

    creacion calculadorar desde casa

---
+++ src/main/java/es/santander/ascentic/proyecto06/Calculadora.java
@@ -0,0 +1,8 @@
+package es.santander.ascentic.proyecto06;
+
+public class Calculadora {
+   public static void main(String[] args) {
+    double resultado;
+    EntradaDeUsuario entradadeusuario = new EntradaDeUsuario(System.in);
+   }
+}
